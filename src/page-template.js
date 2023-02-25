706c2e41b83f7310688f5d499e0230ddc5e94732
Erickuby <ericcnwankwo@gmail.com>
2023-02-25 18:21:40 +0000

    HTML output file created

--- src/page-template.js
+++ src/page-template.js
@@ -97,6 +97,81 @@ module.exports = team => {
     <script src="https://kit.fontawesome.com/c502137733.js"></script>
 </head>
 
+<style>
+    body {
+        background-color: #f5f5f5;
+    }
+
+    .jumbotron {
+        background-color: #2b2d42;
+        color: #fff;
+        text-align: center;
+        padding: 2rem;
+        border-radius: 0;
+    }
+
+    .team-heading {
+        font-size: 3rem;
+        font-weight: 300;
+        margin-bottom: 2rem;
+    }
+
+    .team-area {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin-top: 3rem;
+    }
+
+    .card {
+        background-color: #fff;
+        border-radius: 0.25rem;
+        box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
+        overflow: hidden;
+        margin-bottom: 2rem;
+        width: 18rem;
+        transition: all 0.3s ease;
+    }
+
+    .card:hover {
+        transform: translateY(-0.25rem);
+        box-shadow: 0 0.75rem 1.5rem rgba(0, 0, 0, 0.25);
+    }
+
+    .card-header {
+        background-color: #2b2d42;
+        color: #fff;
+        padding: 0.5rem;
+    }
+
+    .card-body {
+        padding: 1rem;
+    }
+
+    .card-title {
+        font-size: 1.5rem;
+        font-weight: 400;
+        margin-bottom: 0.5rem;
+    }
+
+    .card-text {
+        font-size: 1.25rem;
+        font-weight: 300;
+        margin-bottom: 0.5rem;
+    }
+
+    .card-link {
+        color: #2b2d42;
+        font-weight: 500;
+        transition: all 0.3s ease;
+    }
+
+    .card-link:hover {
+        color: #ff2e63;
+        text-decoration: none;
+    }
+</style>
+
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -113,6 +188,7 @@ module.exports = team => {
         </div>
     </div>
 </body>
+
 </html>
     `;
 };
